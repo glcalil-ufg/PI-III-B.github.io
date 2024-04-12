3b9d0dbbdd7e6ae882ea4accc6cd342de550b1e2
glcalil <glcalil@discente.ufg.br>
2024-04-12 01:08:24 -0300

    feat preenchendo dados do card dinamicamente

---
+++ Card/card.js
@@ -0,0 +1,46 @@
+const Card = {
+
+    class: {
+        title: 'task-title',
+        date: 'task-date',
+        body: 'task-body'
+    },
+
+    renderCard: (data) => {
+        for (const part of Object.keys(Card.class)){
+            const elementPart = document.getElementsByClassName(Card.class[part])[0]
+            elementPart.innerHTML = data[part];
+        }
+    },
+
+    getCardData: () => {
+        const queryParams = {};
+        const queryString = window.location.search.substring(1);
+        const params = queryString.split('&');
+        for (let i = 0; i < params.length; i++) {
+            const pair = params[i].split('=');
+            const key = decodeURIComponent(pair[0]);
+            const value = decodeURIComponent(pair[1]);
+            queryParams[key] = value;
+        }
+
+        const task = Task.getTaskById(queryParams.task, queryParams.list)
+
+        return task;
+    },
+
+    loadcard: () => {
+        const task = Card.getCardData();
+        console.log(task)
+        if(!task.error){
+            Card.renderCard(task.data);
+        } else {
+            alert('Falha ao buscar card');
+            window.location = '../Board/board.html';
+        }
+    },
+
+    start: () => {
+        Card.loadcard();
+    }
+}

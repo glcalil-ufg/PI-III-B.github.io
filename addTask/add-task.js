4e75e7f15c4a080de7755decfb675706c25ca4cf
glcalil <glcalil@discente.ufg.br>
2024-04-12 21:53:51 -0300

    Feat salvar data inserida pelo usuario

--- addTask/add-task.js
+++ addTask/add-task.js
@@ -3,6 +3,7 @@ const AddTask = {
     id: {
         taksForm: "task-form",
         taksInput: 'new-task',
+        dateInput: 'date-task',
         descriptionInput: 'description-task',
         listInput: 'list-select',
     },
@@ -13,26 +14,26 @@ const AddTask = {
         return {
             task: form.getAll(AddTask.id.taksInput)[0], //lê campo name do formulário
             description:  form.getAll(AddTask.id.descriptionInput)[0], //lê campo email do formulário
-            list:  form.getAll(AddTask.id.listInput)[0], //lê campo date do formulário
+            list:  form.getAll(AddTask.id.listInput)[0], //lê campo list do formulário
+            date:  AddTask.getFormatDate(form.getAll(AddTask.id.dateInput)[0]), //lê campo date do formulário
         }
     },
 
-    getCurrentDate: () => {
-        const today = new Date();
-        const year = today.getFullYear();
-        const month = String(today.getMonth() + 1).padStart(2, '0');
-        const day = String(today.getDate()).padStart(2, '0');
-        return `${day}/${month}/${year}`;
+    getFormatDate: (date) => {
+        const part = date.split('-');
+        return `${part[2]}/${part[1]}/${part[0]}`;
     },
 
     clearForm: () => {
         const taskInput = document.getElementById(AddTask.id.taksInput);
         const descriptionInput = document.getElementById(AddTask.id.descriptionInput);
         const listInput = document.getElementById(AddTask.id.listInput);
+        const dateInput = document.getElementById(AddTask.id.dateInput);
         
         // Clear the values of the input elements
         taskInput.value = '';
         descriptionInput.value = '';
+        dateInput.value = '';
         listInput.selectedIndex = 0;
     },
     
@@ -45,8 +46,7 @@ const AddTask = {
             if(data.description.length > 255) {
                 alert('Campos descrição não pode ter mais de 255 caracteres');
             }else {
-                console.log(data);
-                Task.saveTask(data.task, data.description, AddTask.getCurrentDate(), data.list);
+                Task.saveTask(data.task, data.description, data.date, data.list);
 
                 let result = confirm('Task salva com sucesso. Deseja voltar a lista de tarefas?');
 
@@ -60,8 +60,39 @@ const AddTask = {
         });
     },
 
+    dataInputBehavior: () => {
+        let flagfocus = false;
+        const date = document.getElementById(AddTask.id.dateInput);
+
+        date.addEventListener('focus', () => {
+            if(!date.value){
+                date.type = 'date';
+            } 
+            flagfocus = true;
+        });
+
+        date.addEventListener('mouseover', () => {
+            if(!date.value){
+                date.type = 'date';
+            } 
+            date.addEventListener('mouseout', () => {
+                if(!date.value && !flagfocus){
+                    date.type = 'text';
+                } 
+            });
+        });
+
+        date.addEventListener('blur', () => {
+            if(!date.value){
+                date.type = 'text';
+            } 
+            flagfocus = false;
+        });
+    },
+
     start: () => {
         AddTask.listener();
+        AddTask.dataInputBehavior();
     }
 
 }

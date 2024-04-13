192c9aa68a67be798c0b0d1f434f793cafe659f7
glcalil <glcalil@discente.ufg.br>
2024-04-12 23:24:39 -0300

    Feat deletar card

--- Card/card.js
+++ Card/card.js
@@ -6,11 +6,23 @@ const Card = {
         body: 'task-body'
     },
 
+    id: {
+        btnExcluir: 'excluir'
+    },
+
+    global: {
+        taskId: '',
+        taskList: ''
+    },
+
     renderCard: (data) => {
         for (const part of Object.keys(Card.class)){
             const elementPart = document.getElementsByClassName(Card.class[part])[0]
             elementPart.innerHTML = data[part];
         }
+        const btnExcluir = document.getElementById(Card.id.btnExcluir);
+        btnExcluir.setAttribute('data-task', Card.global.taskId);
+        btnExcluir.setAttribute('data-list', Card.global.taskList);
     },
 
     getCardData: () => {
@@ -24,6 +36,8 @@ const Card = {
             queryParams[key] = value;
         }
 
+        Card.global.taskId = queryParams.task;
+        Card.global.taskList = queryParams.list;
         const task = Task.getTaskById(queryParams.task, queryParams.list)
 
         return task;
@@ -38,6 +52,13 @@ const Card = {
             alert('Falha ao buscar card');
             window.location = '../Board/board.html';
         }
+    },
+
+    deleteTask: (element) => {
+        const taskId = element.dataset.task;
+        const tasklist = element.dataset.list;
+        Task.deleteTaskById(taskId, tasklist);
+        window.location = '../Board/board.html';
     },
 
     start: () => {
